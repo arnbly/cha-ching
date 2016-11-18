de72792b12642ec18454bd2ac1c256d7a5a7027c
Aaron Bailey <aaron@earnest.com>
2016-11-17 18:35:58 -0800

    Test

---
+++ Split-It Wireframes/Split-It Wireframe/FilterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FilterViewController.swift
+//  Split-It Wireframe
+//
+//  Created by Lauren Tindal on 11/17/16.
+//  Copyright © 2016 Lauren Tindal. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func didPressBack(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
