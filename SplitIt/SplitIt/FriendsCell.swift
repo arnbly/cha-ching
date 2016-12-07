370217e59eafdc841cb78c45f2382ce02dc2c9c6
Lauren <ltindal19@gmail.com>
2016-12-06 19:50:40 -0800

    big merge
    hope this works

---
+++ SplitIt/SplitIt/FriendsCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsCell.swift
+//  SplitIt
+//
+//  Created by Lauren Tindal on 12/3/16.
+//  Copyright © 2016 Aaron Bailey. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var selectDefault: UIImageView!
+    @IBOutlet weak var selectActive: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SplitIt/SplitIt/NewTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  NewTableViewCell.swift
+//  SplitIt
+//
+//  Created by Lauren Tindal on 12/5/16.
+//  Copyright © 2016 Aaron Bailey. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userPayment: UITextField!
+    @IBOutlet weak var userName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
