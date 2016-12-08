8bf84def75dd697bf784ca86ab52b67bdd11833c
writetoamrutha <writetoamrutha@gmail.com>
2016-12-07 22:39:19 -0800

    SignIn and Settings - merge 3

--- SplitIt/SplitIt/SignInViewController.swift
+++ SplitIt/SplitIt/SignInViewController.swift
@@ -37,12 +37,12 @@ class SignInViewController: UIViewController, FBSDKLoginButtonDelegate {
         
         //Firebase get the currently signed in user
         FIRAuth.auth()?.addStateDidChangeListener({ (auth, user) in
-         if user != nil {
-         //User is signed in
-         } else {
-         //No user is signed in
-         }
-         })
+            if user != nil {
+                //User is signed in
+            } else {
+                //No user is signed in
+            }
+        })
     }
     
     func handleCustomFBLogin() {
@@ -79,130 +79,134 @@ class SignInViewController: UIViewController, FBSDKLoginButtonDelegate {
         FIRAuth.auth()?.signIn(with: credential) { (user, error) in
             // ...
             if error != nil {
-                print ("Something went wrong", error)
+                print ("Something went wrong", error as Any)
                 return
-
+                
             }
-            print ("Success with FIR", user)
-        
-        
+            print ("Success with FIR", user as Any)
+            
+            
         }
         
-        /*
-         let accessToken = FBSDKAccessToken.current()
-         guard let accessTokenString = accessToken?.tokenString else {return}
-         let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
-         FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
-         if error != nil {
-         print ("Something went wrong", error)
-         return
-         }
-         print("Success with FIR", user)
-         let ref = FIRDatabase.database().reference(fromURL: "https://splititfirebase.firebaseio.com/")
-         
-         // Guard for user id
-         guard let uid = user?.uid else {
-         return
-         }
-         
-         // Create a child reference - uid will let us wrap each users data in a unique user id for later reference
-         let usersReference = ref.child("users").child(uid)
-         */
         
-        FBSDKGraphRequest(graphPath: "/me", parameters:
-            ["fields": "id, name, email, picture.type(large)"]).start { (connection, result, err) in
-                
-                if err != nil {
-                    print("Failed to start Graph Request", err)
-                    return
-                }
-                else {
-                    if let dataDict = result as? [String:AnyObject] {
-                        
-                        
-                        // Photo URL code
-                        let picture = dataDict["picture"] as? [String:AnyObject]
-                        let data = picture?["data"] as? NSDictionary
-                        let URL = data?["url"] as? String
-                        let pictureURL = NSURL(string: URL!)
-                        let imageData = NSData(contentsOf: pictureURL as! URL)
-                        let profileImage = UIImage(data: imageData as! Data)
-                        
-                        
-                        // Name code
-                        let username = dataDict["name"]
-                        var name = String()
-                        name = username as! String
-                        
-                        // Email code
-                        let useremail = dataDict["email"]
-                        var email = String()
-                        email = useremail as! String
-                        
-                        var profilePictureURL = String()
-                        profilePictureURL = URL!
-                        
-                        /*
-                         var userCurrency = String()
-                         userCurrency = "USD"
-                         */
-                        
-                        print("User Name is: \(username)")
-                        print("User Email is: \(useremail)")
-                        print("User Image URL is: \(pictureURL)")
-                        
-                        
-                        
-                        /*  let values = ["displayName": name, "email": email, "photoURL": profilePictureURL] as [String : Any]
-                         
-                         
-                         
-                         //Update our databse by using the child database reference above called usersReference
-                         usersReference.setValue(values, withCompletionBlock: { (err, ref) in
-                         
-                         if err != nil {
-                         print(err)
-                         return
-                         }
-                         
-                         
-                         
-                         print(ref)
-                         
-                         print("Save the user successfully into Firebase database")
-                         })
-                         */
-                        
-                        
-                        
-                        //Passing data to Settings View Controller
-                        /*
-                         let settingsstoryboard = UIStoryboard(name: "Settings", bundle: nil)
-                         let settingscontroller = settingsstoryboard.instantiateViewController(withIdentifier: "SettingsSB") as! SettingsViewController
-                         
-                         //  settingscontroller.name = username as! String
-                         // settingscontroller.email = useremail as! String
-                         // settingscontroller.profilePicture = profileImage!
-                         
-                         self.present(settingscontroller, animated: true, completion: nil)
-                       */
+        
+        
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else {return}
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error != nil {
+                print ("Something went wrong", error as Any)
+                return
+            }
+            print("Success with FIR", user as Any)
+            let ref = FIRDatabase.database().reference(fromURL: "https://splitit-bb5bc.firebaseio.com/")
+            
+            // Guard for user id
+            guard let uid = user?.uid else {
+                return
+            }
+            
+            // Create a child reference - uid will let us wrap each users data in a unique user id for later reference
+            let usersReference = ref.child("users").child(uid)
+            
+            
+            
+            
+            FBSDKGraphRequest(graphPath: "/me", parameters:
+                ["fields": "id, name, email, picture.type(large)"]).start { (connection, result, err) in
+                    
+                    if err != nil {
+                        print("Failed to start Graph Request", err as Any)
+                        return
+                    }
+                    else {
+                        if let dataDict = result as? [String:AnyObject] {
+                            
+                            
+                            // Photo URL code
+                            let picture = dataDict["picture"] as? [String:AnyObject]
+                            let data = picture?["data"] as? NSDictionary
+                            let URL = data?["url"] as? String
+                            let pictureURL = NSURL(string: URL!)
+                            let imageData = NSData(contentsOf: pictureURL as! URL)
+                            let profileImage = UIImage(data: imageData as! Data)
+                            
+                            
+                            // Name code
+                            let username = dataDict["name"]
+                            var name = String()
+                            name = username as! String
+                            
+                            // Email code
+                            let useremail = dataDict["email"]
+                            var email = String()
+                            email = useremail as! String
+                            
+                            var profilePictureURL = String()
+                            profilePictureURL = URL!
+                            
+                            
+                             var userCurrency = String()
+                             userCurrency = "USD"
+                            
+                            
+                            print("User Name is: \(username)")
+                            print("User Email is: \(useremail)")
+                            print("User Image URL is: \(pictureURL)")
+                            
+                            
+                            
+                            let values = ["displayName": name, "email": email, "photoURL": profilePictureURL, "currency":userCurrency] as [String : Any]
+                            
+                            
+                            
+                            //Update our databse by using the child database reference above called usersReference
+                            usersReference.setValue(values, withCompletionBlock: { (err, ref) in
+                                
+                                if err != nil {
+                                    print(err as Any)
+                                    return
+                                }
+                                
+                                
+                                
+                                print(ref)
+                                
+                                print("---> Save the user successfully into Firebase database")
+                            })
+                            
+                            
+                            
+                            
+                            //Passing data to Settings View Controller
+                            
+                            let settingsstoryboard = UIStoryboard(name: "Settings", bundle: nil)
+                            let settingscontroller = settingsstoryboard.instantiateViewController(withIdentifier: "SettingsSB") as! SettingsViewController
+                            
+                            //  settingscontroller.name = username as! String
+                            // settingscontroller.email = useremail as! String
+                            // settingscontroller.profilePicture = profileImage!
+                            
+                            self.present(settingscontroller, animated: true, completion: nil)
+                            
  
+                            
+                        }
+                        
+                        print(result as Any)
                         
                     }
-                    
-                    print (result)
-                    
-                }
-        }
-        
-        self.performSegue(withIdentifier: "welcomePageSegue", sender: nil)
+            }
+            
+            //  self.performSegue(withIdentifier: "welcomePageSegue", sender: nil)
+            
+        })
         
     }
     
+    
 }
-
-
-
 
 
 
